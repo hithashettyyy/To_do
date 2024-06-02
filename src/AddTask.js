6ab54919c1d38abcb783ee23d03e2278c5849ec8
Hitha R Shetty <hitharshettyyy@gmail.com>
2024-06-02 12:15:09 +0530

    routing enabled

--- src/AddTask.js
+++ src/AddTask.js
@@ -9,13 +9,17 @@ function AddTask(props) {
       <div className="add_container">
         <input className="add-task-input" type="text" placeholder="Enter a new task......" value={props.currentInput} onChange={props.handleInput}></input>
         <button className="add" onClick={props.handleClick}> Add </button>
-      </div>
-      {props.taskAdded && 
-        <div className="message-container">
-           <p>New Task Added Successfully</p>
-           <button onClick={props.closeButton}>x</button>
         </div>
-     }
+
+        <div className="message">
+        {props.taskAdded && 
+          <div className='message-container'>
+           <button className='close' onClick={props.closeButton}>x</button>
+           <p>New Task Added Successfully</p>
+          </div>
+        }
+        </div>
+        
     </>
 
   )
